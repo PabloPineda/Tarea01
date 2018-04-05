7ce9291c598cb784e477c64cc4d3a9eb612fe581
Pablo Lopez <00123617@uca.edu.sv>
2018-04-04 19:19:04 -0600

    Primer tarea

---
+++ src/tarea01/Tarea01.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea01;
+
+/**
+ *
+ * @author Pablo Lopez
+ */
+public class Tarea01 {
+
+    /**
+     * @param args the command line arguments
+     */
+     public static void main(String[] args) {
+        System.out.println("    -DATOS GENERALES-");
+        System.out.println("*-*-*-*-*-*-*-*-*-*-*-**-*-*-*--**-*--*");
+        System.out.println("Nombre: Pablo José.");
+        System.out.println("Apellidos: López Pineda.");
+        System.out.println("Edad: 17 anios.");
+        System.out.println("Fecha de nacimiento:19 de abril de 2000.");
+        System.out.println("Carné estudiantil: 00123617.");
+        System.out.println("-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*");
+    }
+    
+}
